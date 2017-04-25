c6983411f03ff410a289ab1d4c51d2dd8ea6df79
lukassos <lukassos@prophet.here>
2017-04-25 10:29:09 +0100

    done setion 4 of tutorial

--- alpha_blog/app/controllers/articles_controller.rb
+++ alpha_blog/app/controllers/articles_controller.rb
@@ -1,5 +1,6 @@
 class ArticlesController < ApplicationController
-  
+  before_action :set_article, only: [:edit, :update, :show, :destroy]  
+ 
   def index
     @article_list = Article.all
   end
@@ -23,11 +24,11 @@ class ArticlesController < ApplicationController
   end
   
   def edit
-    @article = Article.find(params[:id])
+    #@article = Article.find(params[:id])
   end
 
   def update
-    @article = Article.find(params[:id])
+    #@article = Article.find(params[:id])
     if @article.update(article_params)
       flash[:notice] = "Article was succesfully updated"
       redirect_to article_path(@article)
@@ -37,11 +38,11 @@ class ArticlesController < ApplicationController
   end
 
   def show
-    @article = Article.find(params[:id]) 
+    #@article = Article.find(params[:id]) 
   end
 
   def destroy
-    @article = Article.find(params[:id])
+    #@article = Article.find(params[:id])
     @article.destroy
     flash[:notice] = "Article was succesfully destroyed"
     redirect_to articles_path
@@ -51,5 +52,7 @@ class ArticlesController < ApplicationController
     def article_params
       params.require(:article).permit(:title,:description)
     end
-
+    def set_article
+      @article = Article.find(params[:id])
+    end
 end
