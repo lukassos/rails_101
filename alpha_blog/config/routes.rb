fa3d3f75c55cec3b6ad840a1cce530db2a9e3df5
lukassos <lukassos@prophet.here>
2017-04-25 10:05:39 +0100

    main_pages + root page + menu links partial

--- alpha_blog/config/routes.rb
+++ alpha_blog/config/routes.rb
@@ -2,4 +2,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :articles
 
+  root 'main_pages#home'
+
+  get 'home', to: 'main_pages#home'
+
+  get 'about', to: 'main_pages#about'
+
 end
