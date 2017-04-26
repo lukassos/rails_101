5d25145ce136d201bfb2f4d7190e89b3e755c997
lukassos <lukassos@prophet.here>
2017-04-27 00:35:16 +0100

    styling messages and alerts

--- alpha_blog/app/controllers/articles_controller.rb
+++ alpha_blog/app/controllers/articles_controller.rb
@@ -12,7 +12,7 @@ class ArticlesController < ApplicationController
   def create
     @article = Article.new(article_params)
     if @article.save
-      flash[:notice] = "Article was succesfully created"
+      flash[:success] = "Article was succesfully created"
       redirect_to article_path(@article)
     else
       render 'new'
@@ -30,7 +30,7 @@ class ArticlesController < ApplicationController
   def update
     #@article = Article.find(params[:id])
     if @article.update(article_params)
-      flash[:notice] = "Article was succesfully updated"
+      flash[:success] = "Article was succesfully updated"
       redirect_to article_path(@article)
     else
       render 'edit'
@@ -44,7 +44,7 @@ class ArticlesController < ApplicationController
   def destroy
     #@article = Article.find(params[:id])
     @article.destroy
-    flash[:notice] = "Article was succesfully destroyed"
+    flash[:danger] = "Article was succesfully destroyed"
     redirect_to articles_path
   end
 
